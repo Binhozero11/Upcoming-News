911a8ee19bb50f2f00a19356e397b2dc2699b449
Fábio de Jesus Xavier <123664808+Binhozero11@users.noreply.github.com>
2024-12-13 12:58:15 +0000

    Adicionando a pasta pages

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function Home() {
+return <h2>Parabéns, você ganhou!! agora deposite R$ 1 milhão na minha conta e receba felicidade comendo doces comigo hahaha ou apenas sorria, pois você já é muito feliz!!</h2>
+
+}
+
+export default Home;
