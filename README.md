9d7485a13d66b318f92a312846c7f38df65cf28c
Fábio de Jesus Xavier <123664808+Binhozero11@users.noreply.github.com>
2024-12-20 17:50:35 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Upcoming-News
+
 Pequeno clone do site https://www.tabnews.com.br com o curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
 function Home() {
-return <h2>Parabéns, você ganhou!! agora deposite R$ 1 milhão na minha conta e receba felicidade comendo doces comigo hahaha ou apenas sorria, pois você já é muito feliz!!</h2>
+  return (
+    <h2>
+      Parabéns, você ganhou!! agora deposite R$ 1 milhão na minha conta e receba
+      felicidade comendo doces comigo hahaha ou apenas sorria, pois você já é
+      muito feliz!!
+    </h2>
+  );
+}
 
+function nd() {
+  console.log("testando");
 }
 
 export default Home;
